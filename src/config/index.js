e0c6f736f5be31a432b996c7ee314f160cf95067
babycmd <babycmd@baby.baby>
2022-01-20 22:49:09 -0300

    Add index.html

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+const config = {
+        //contractAddress : "KT1AmPpGN9g1ew29xYDRSyD7FCVSJ44xizye", 
+        //tokenAddress: "KT1TnBDd6YmYVUysUCq1CRb85B4yPhE2W7qR"
+
+        contractAddress : "KT1DtXLu9UCQtNYHSqTBK5xfWcW6HYTDqyGF", 
+        tokenAddress: "KT1JQ2VscxLQszkVBteCQaHaVf163csaZmVg"
+        
+}
+
+export default config;
